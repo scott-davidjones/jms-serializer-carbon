d52b81dadd705de659fc2109380a813063ebcd6c
Matěj Račinský <matej.racinsky@avast.com>
2021-01-06 22:38:19 +0100

    refactored, not passing unnecessary parameters, support for CarbonImmutable

--- src/AutumnDev/JMS/CarbonHandler.php
+++ src/AutumnDev/JMS/CarbonHandler.php
@@ -2,16 +2,14 @@
 
 namespace AutumnDev\JMS;
 
-use Carbon\CarbonImmutable;
+use Carbon\CarbonInterface;
 use JMS\Serializer\GraphNavigatorInterface;
 use JMS\Serializer\Handler\SubscribingHandlerInterface;
-use JMS\Serializer\VisitorInterface;
-use JMS\Serializer\JsonSerializationVisitor;
+use JMS\Serializer\Visitor\SerializationVisitorInterface;
 use JMS\Serializer\XmlSerializationVisitor;
 use JMS\Serializer\JsonDeserializationVisitor;
 use JMS\Serializer\XmlDeserializationVisitor;
 use JMS\Serializer\Context;
-use JMS\Serializer\Handler\DateHandler;
 use Carbon\Carbon;
 use DateTimeImmutable;
 use DateTimeZone;
@@ -36,142 +34,99 @@ class CarbonHandler implements SubscribingHandlerInterface
 	}
 
 	public static function getSubscribingMethods()
-    {
-        $methods = [];
-        $deserialisationTypes = ['Carbon', 'CarbonImmutable'];
-        $serialisationTypes = ['Carbon', 'CarbonImmutable'];
+	{
+		$methods = [];
+		$deserialisationTypes = ['Carbon', 'CarbonImmutable'];
+		$serialisationTypes = ['Carbon', 'CarbonImmutable'];
 
-        foreach (['json', 'xml', 'yml'] as $format) {
+		foreach (['json', 'xml', 'yml'] as $format) {
 
-            foreach ($deserialisationTypes as $type) {
-                $methods[] = [
-                    'type'      => $type,
-                    'direction' => GraphNavigatorInterface::DIRECTION_DESERIALIZATION,
-                    'format'    => $format,
-                ];
-            }
+			foreach ($deserialisationTypes as $type) {
+				$methods[] = [
+					'type' => $type,
+					'direction' => GraphNavigatorInterface::DIRECTION_DESERIALIZATION,
+					'format' => $format,
+				];
+			}
 
-            foreach ($serialisationTypes as $type) {
-                $methods[] = [
-                    'type' => $type,
-                    'format' => $format,
-                    'direction' => GraphNavigatorInterface::DIRECTION_SERIALIZATION,
-                    'method' => 'serialize'.$type,
-                ];
-            }
-        }
+			foreach ($serialisationTypes as $type) {
+				$methods[] = [
+					'type' => $type,
+					'format' => $format,
+					'direction' => GraphNavigatorInterface::DIRECTION_SERIALIZATION,
+					'method' => 'serializeCarbon',
+				];
+			}
+		}
 
-        return $methods;
-    }
+		return $methods;
+	}
 
-    /**
-     * serializes carbon object to ISO 8601 string for JSON
-     * serialization
-     *
-     * @param XmlSerializationVisitor $visitor
-     * @param Carbon $date
-     * @param array $type
-     * @param Context $context
-     *
-     * @return String
-     */
-    public function serializeCarbon(
-        VisitorInterface $visitor,
-        Carbon $date,
-        array $type,
-        Context $context
-    ) {
-        if ($visitor instanceof XmlSerializationVisitor && false === $this->xmlCData) {
-            return $visitor->visitSimpleString($date->format($this->getFormat($type)), $type, $context);
-        }
-
-        $format = $this->getFormat($type);
-        if ('U' === $format) {
-            return $visitor->visitInteger($date->format($format), $type, $context);
-        }
-
-        return $visitor->visitString($date->format($this->getFormat($type)), $type, $context);
-    }
-
-	/**
-	 * serializes carbon immutable object to ISO 8601 string for JSON
-	 * serialization
-	 *
-	 * @param XmlSerializationVisitor $visitor
-	 * @param CarbonImmutable $date
-	 * @param array $type
-	 * @param Context $context
-	 *
-	 * @return String
-	 */
-	public function serializeCarbonImmutable(
-		VisitorInterface $visitor,
-		CarbonImmutable $date,
+	public function serializeCarbon(
+		SerializationVisitorInterface $visitor,
+		CarbonInterface $date,
 		array $type,
 		Context $context
-	) {
+	): string
+	{
 		if ($visitor instanceof XmlSerializationVisitor && false === $this->xmlCData) {
-			return $visitor->visitSimpleString($date->format($this->getFormat($type)), $type, $context);
+			return $visitor->visitSimpleString($date->format($this->getFormat($type)), $type);
 		}
 
 		$format = $this->getFormat($type);
 		if ('U' === $format) {
-			return $visitor->visitInteger($date->format($format), $type, $context);
+			return $visitor->visitInteger($date->format($format), $type);
 		}
 
-		return $visitor->visitString($date->format($this->getFormat($type)), $type, $context);
+		return $visitor->visitString($date->format($this->getFormat($type)), $type);
 	}
 
-    public function deserializeCarbonFromXml(XmlDeserializationVisitor $visitor, $data, array $type)
-    {
-        if ($this->isDataXmlNull($data)) {
-            return null;
-        }
+	public function deserializeCarbonFromXml(XmlDeserializationVisitor $visitor, $data, array $type)
+	{
+		if ($this->isDataXmlNull($data)) {
+			return null;
+		}
 
-        $dateObj = $this->parseDateTime($data, $type);
-        return Carbon::instance($dateObj);
-    }
+		$dateObj = $this->parseDateTime($data, $type);
+		return Carbon::instance($dateObj);
+	}
 
-    public function deserializeCarbonFromJson(JsonDeserializationVisitor $visitor, $data, array $type)
-    {
-        if (empty($data)) {
-            return null;
-        }
+	public function deserializeCarbonFromJson(JsonDeserializationVisitor $visitor, $data, array $type)
+	{
+		if (empty($data)) {
+			return null;
+		}
 
-        $dateObj = $this->parseDateTime($data, $type);
-        return Carbon::instance($dateObj);
-    }
+		$dateObj = $this->parseDateTime($data, $type);
+		return Carbon::instance($dateObj);
+	}
 
-    private function parseDateTime($data, array $type, $immutable = false)
-    {
-        $timezone = isset($type['params'][1]) ? new DateTimeZone($type['params'][1]) : $this->defaultTimezone;
-        $format = $this->getFormat($type);
+	private function parseDateTime($data, array $type, $immutable = false)
+	{
+		$timezone = isset($type['params'][1]) ? new DateTimeZone($type['params'][1]) : $this->defaultTimezone;
+		$format = $this->getFormat($type);
 
-        if ($immutable) {
-            $datetime = DateTimeImmutable::createFromFormat($format, (string) $data, $timezone);
-        } else {
-            $datetime = DateTime::createFromFormat($format, (string) $data, $timezone);
-        }
+		if ($immutable) {
+			$datetime = DateTimeImmutable::createFromFormat($format, (string)$data, $timezone);
+		} else {
+			$datetime = DateTime::createFromFormat($format, (string)$data, $timezone);
+		}
 
-        if (false === $datetime) {
-            throw new RuntimeException(sprintf('Invalid datetime "%s", expected format %s.', $data, $format));
-        }
+		if (false === $datetime) {
+			throw new RuntimeException(sprintf('Invalid datetime "%s", expected format %s.', $data, $format));
+		}
 
-        return $datetime;
-    }
+		return $datetime;
+	}
 
-    /**
-     * @return string
-     * @param array $type
-     */
-    private function getFormat(array $type)
-    {
-        return isset($type['params'][0]) ? $type['params'][0] : $this->defaultFormat;
-    }
-    
-    private function isDataXmlNull($data)
-    {
-        $attributes = $data->attributes('xsi', true);
-        return isset($attributes['nil'][0]) && (string)$attributes['nil'][0] === 'true';
-    }
+	private function getFormat(array $type): string
+	{
+		return isset($type['params'][0]) ? $type['params'][0] : $this->defaultFormat;
+	}
+
+	private function isDataXmlNull($data): bool
+	{
+		$attributes = $data->attributes('xsi', true);
+		return isset($attributes['nil'][0]) && (string)$attributes['nil'][0] === 'true';
+	}
 }
